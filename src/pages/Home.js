087580eb0a040f71620f0c07340862a770c35bb9
Remco <Remcolangemaire@gmail.com>
2022-03-28 23:13:25 +0200

    Revert "Revert "update homepage""
    This reverts commit 41af42159a7f2f1984c5d1f01a3a3af977ac09ca.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,12 @@ import React from "react";
 import './home.scss';
 import HomeMap from "../components/homeMap/HomeMap";
 import AboutUs from "../components/aboutUs/AboutUs";
+import Front from "../components/frontPage/Front";
 
 const Home = () => {
     return (
         <div className="hero-container">
+            <Front/>
             <HomeMap/>
             <AboutUs/>
         </div>
