fa8d23fab8ee3f5980136a413ad73d07aec0641f
Remco <Remcolangemaire@gmail.com>
2022-03-28 21:31:16 +0200

    Update HomeMap.js

--- src/components/homeMap/HomeMap.js
+++ src/components/homeMap/HomeMap.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import './homeMap.scss'
 
-import '../cards/Card1'
-import Card1 from '../cards/Card1'
-
 const HomeMap = () => {
   return (
     <div className='map-hero-container'>
         <h1>Hier komt de map</h1>
         <p>lol dit is onzin</p>
-        <Card1/>
     </div>
   )
 }
