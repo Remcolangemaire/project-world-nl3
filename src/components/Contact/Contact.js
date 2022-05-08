5debef16d5af6c0118186694bdb48c2184aa1821
Remco <Remcolangemaire@gmail.com>
2022-05-08 14:03:54 +0200

    latest

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './contact.scss'
+
+const Contact = () => {
+  return (
+    <div>
+        <h1>Contact us</h1>
+        <p>lorem ipsum</p>
+
+        
+    </div>
+  )
+}
+
+export default Contact
