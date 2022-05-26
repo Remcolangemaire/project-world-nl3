f021ba326c7798ae78db0f6f74ecfe8e2ef22ab9
Remco <Remcolangemaire@gmail.com>
2022-05-26 22:36:53 +0200

    veel

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './contact.scss'
+import { MdEmail, MdCall, MdLocationOn, MdFacebook } from "react-icons/md";
 
 const Contact = () => {
   return (
@@ -7,26 +8,45 @@ const Contact = () => {
         <h1>Contact us</h1>
         <p>lorem ipsum</p>
 
-        <div id='contact-container'>
-          <div className='contact-info'>
-          <h4>Contact information</h4>
-            <p>text</p>
-            <div className='icon-text'>
-            <i class="fa fa-phone" aria-hidden="true"></i>
-              <span>text</span>
+        <div id="contact-container">
+          <div className="contact-info">
+            <h4>
+            Contact Information
+            </h4>
+            <p>Fill up the form and then click send</p>
+            <div className="icon-text">
+              <MdCall/>
+              <span>+31-623493617</span>
+              <MdCall/>
+              <span>+31-610695887</span>
             </div>
-            <div className='social-media'>
-              <a href='#' className='icon-circle'>
-                <i className='icon'></i>
+            <div className="icon-text">
+              <MdEmail/>
+              <span>admin@billys.com</span>
+            </div>
+            <div className="icon-text">
+              <MdLocationOn/>
+              <span>435 Grand Ave,Ridgewood,NY 11385</span>
+            </div>
+            <div className="social-media">
+              <a href="#" className="icon-circle">
+                <MdFacebook className='iconsize'/>
+              </a>
+              <a href="#" className="icon-circle">
+                <MdFacebook className='iconsize'/>
+              </a>
+              <a href="#" className="icon-circle">
+                <MdFacebook className='iconsize'/>
               </a>
             </div>
-          </div>
-          <form>
-            
-          </form>
         </div>
+        <form>
+
+        </form>
+      </div>
     </>
   )
 }
 
 export default Contact
+
