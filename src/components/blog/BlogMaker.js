7b6f66b4c4743df709e7565ed0bf512780a82a74
Remcolangemaire <Remcolangemaire@gmail.com>
2022-06-09 13:16:34 +0200

    fix

---
+++ src/components/blog/BlogMaker.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BlogMaker() {
+  return (
+    <div>BlogMaker</div>
+  )
+}
+
+export default BlogMaker
---
+++ src/components/blog/PostText.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+
+function PostText(props) {
+  const { post } = props;
+
+  return (
+    <Card sx={{ display: 'flex' }}>
+        <CardContent sx={{ flex: 1 }}>
+        <Typography component="h2" variant="h5">
+            {post.title}
+        </Typography>
+        <Typography variant="subtitle1" color="text.secondary">
+            {post.date}
+        </Typography>
+        <Typography variant="subtitle1" paragraph>
+            {post.description}
+        </Typography>
+        <Typography variant="subtitle1" color="primary">
+            Continue reading...
+        </Typography>
+        </CardContent>
+        <CardMedia
+        component="img"
+        sx={{ width: 160, display: { xs: 'none', sm: 'block' } }}
+        image={post.image}
+        alt={post.imageLabel}
+        />
+    </Card>
+  );
+}
+
+FeaturedPost.propTypes = {
+  post: PropTypes.shape({
+    date: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    imageLabel: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default PostText;
