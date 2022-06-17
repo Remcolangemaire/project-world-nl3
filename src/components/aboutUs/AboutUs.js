beb1141ccc1cd31aed182d40d3aebe5e81db5ecb
Remcolangemaire <Remcolangemaire@gmail.com>
2022-06-17 19:47:52 +0200

    about us foto's update

--- src/components/aboutUs/AboutUs.js
+++ src/components/aboutUs/AboutUs.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 import './aboutUs.scss'
 import Carousel1 from '../cards/Carousel1'
 import { Row } from 'react-bootstrap'
-import profimage1 from '../../images/IMG_7993.png'
-import profimage2 from '../../images/IMG_8272.png'
+import profimage1 from '../../images/IMG_7993_small-crop.png'
+import profimage2 from '../../images/IMG_8272-min-crop.png'
 
 const AboutUs = () => {
   const [number, setNuber] = useState(0);
@@ -11,11 +11,23 @@ const AboutUs = () => {
     <div className='about-hero-container imgas'>
       <Row className="content-containers">
         <div className="wrapper imgass">
-          <div onClick={() => setNuber(0)}>          
-            <img src={profimage1} alt='Remco'/>
+          <div onClick={() => setNuber(0)}>    
+          <div class="carde">
+              <img src={profimage1} alt='Remco'/>
+              <div class="details">
+                <h4>Click here</h4>
+                <p>For more info on Remco</p>
+              </div>
+            </div>        
           </div>
-          <div onClick={() => setNuber(1)}>          
-            <img src={profimage2} alt='Jord'/>
+          <div onClick={() => setNuber(1)}>
+            <div class="carde">
+              <img src={profimage2} alt='Jord'/>
+              <div class="details">
+                <h4>Click here</h4>
+                <p>For more info on Jord</p>
+              </div>
+            </div>          
           </div>
         </div>
       </Row>
